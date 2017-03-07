cc814196a5e3fb42164da34df602f27c9e0409b9
Daniele Sabre <daniele.sabre@gmail.com>
2017-03-07 13:49:34 +0100

    Added /launch command

--- execute.php
+++ execute.php
@@ -3,7 +3,7 @@ $content = file_get_contents("php://input");
 $update = json_decode($content, true);
 
 if(!$update){
-  exit;
+	exit;
 }
 
 $message = isset($update['message']) ? $update['message'] : "";
@@ -18,7 +18,30 @@ $text = isset($message['text']) ? $message['text'] : "";
 $text = trim($text);
 $text = strtolower($text);
 
+$response = '';
+
+// launch command
+if(preg_match('/^\/launch/', $text)){
+	$text = str_replace('/launch', '', $text);
+	$text = trim($text);
+	$info = explode('d', $text);
+	$countLaunches = (int)$info[0];
+	$diceType = (int)$info[1];
+	
+	$results = [];
+	$total = 0;
+	for($i = 0; $i < $countLaunches; $i++){
+		$result = (int)rand(1, $diceType);
+		$results[] = $result;
+		
+		$total += $result;
+	}
+	
+	$results = implode(', ', $results);
+	$response = sprintf("Results: %s%sTotal: %s" , $results, PHP_EOL, $total);
+}
+
 header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => json_encode($update));
+$parameters = array('chat_id' => $chatId, "text" => $response);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
