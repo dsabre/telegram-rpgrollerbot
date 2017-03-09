b20c7b121149dbc5d5c4b7ea1df42002d21d48ad
Daniele Sabre <daniele.sabre@gmail.com>
2017-03-09 09:46:58 +0100

    Removed test code :D

--- execute.php
+++ execute.php
@@ -3,8 +3,6 @@
 require_once 'classes/RPGRollerBot.php';
 
 $input = file_get_contents("php://input");
-$input = '{"update_id":364632588,"message":{"message_id":56,"from":{"id":30343769,"first_name":"Daniele","last_name":"Sabre","username":"Wuzzifuzz"},"chat":{"id":30343769,"first_name":"Daniele","last_name":"Sabre","username":"Wuzzifuzz","type":"private"},"date":1488908870,"text":"/start"}}';
-$input = '';
 
 try{
 	$rpgrollerbot = new RPGRollerBot($input);
